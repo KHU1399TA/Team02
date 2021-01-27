bb0d18bd58165953df5c52253a1860d7de41d4b5
Mjavadrahimi <manre80@gmail.com>
2021-01-27 16:58:09 +0330

    basic main v2

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,33 +1,37 @@
 package main ;
 import java.util.* ;
 public class Main {
+    public static void save(){
 
-    public static void khat(){
+    }
+    public static void load(){
+
+    }
+    public static void line(){
         System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
     }
     public static Scanner scanner = new Scanner(System.in);
-    public static java.util.Date myclock = new java.util.Date();
-
+    public static java.util.Date myClock = new java.util.Date();
+    //public static Restaurant restaurant = new Restaurant();
     public static void main(String[] agrs){
-
         System.out.println("Welcome");
-        khat();
+        line();
 
-        while (true) {
+        while (true) { //start page
 
-            System.out.print("type your number: | login = 0   registeer = 1   exit = 2| :");
+            System.out.print("type your number: | login = 0   registeer = 1   back to start = 2| :");
             int start = scanner.nextInt();
 
             if(start==0){
                 while (true){
                     // get username & password
                     // search in users
-                    // open panel(with method) or print error or go back to start(with break)
+                    // open panel(with method) or print error or go back to start
                 }
             }
             else if(start==1){
                 while (true){
-                    // for client: get f/n name & number & address & username & password(2x) | generate dates(regis & lastlog)
+                    // (for client) get f/n name & number & address & username & password(2x) | generate dates(regis & lastlog)
 
                 }
             }
@@ -35,45 +39,47 @@ public class Main {
                 break;
             }
             else{
-                System.out.println("error");
+                System.out.print("please enter correct number : ");
             }
 
         }
-        khat();
+        line();
         System.out.println("Have a nice day");
-        khat();
+        line();
     }
     void clientpanel(/*User client*/){
         /*
         show menu
-        can make & revoke order ==> action result
+        refresh menu
+        Action result make order(Order order ,Restaurant restaurant)
+        Action result revoke order(revoke order ,Restaurant restaurant)
         check my orders
          */
     }
     void cashierpanel(/*User cashier*/){
         /*
-        check orders and change order state
+        Action result confirmOrder(int id ,Restaurant restaurant)
          */
     }
     void chefpanel(/*User chef*/){
         /*
-        add food(Food food)
-        edit food(int id)
-        remove food(int id)
-        change food status(int id ,boolean isAvailble)
-        cook (int id) (check order if state=confirm)
+        ActionResult add food(Food food ,Restaurant restaurant)
+        ActionResult edit food(int id ,Restaurant restaurant)
+        ActionResult remove food(int id ,Restaurant restaurant)
+        ActionResult change food status(int id ,boolean isAvailble ,Restaurant restaurant)
+        ActionResult cook (int id ,Restaurant restaurant) (check order if state=confirm)
          */
     }
     void deliverymanpanel(/*User deliveryman*/){
         /*
-        deliver (int id) (if state==cooked change to delivered)
+        ActionResult deliver (int id ,Restaurant restaurant) (if state==cooked change to delivered)
          */
     }
     void managerpanel(/*User manager*/){
         /*
-        register ()(rank except client)(add to files and reload array)
-        edit (user name and password)
-        remove
+        ActionResult register (User member ,Restaurant restaurant)(ranks except client)(add to files and reload array)
+        ActionResult edit (String username ,String password ,Restaurant restaurant)
+        ActionResult remove (String username,Restaurant restaurant)
          */
     }
 }
